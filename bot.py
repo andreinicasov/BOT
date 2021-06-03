668085196bcbcd5379719cff3177c4d03f6b7019
andreinicasov <81997122+andreinicasov@users.noreply.github.com>
2021-06-03 14:01:32 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types
+
+# log
+logging.basicConfig(level=logging.INFO)
+
+# handlers
+async def start(message: types.Message):
+	await message.reply('Привет, {0}!'.format(message.from_user.first_name))
+
+async def echo(message: types.Message):
+	await message.answer(message.text)
+
+# Selectel Lambda funcs
+async def register_handlers(dp: Dispatcher):
+
+	dp.register_message_handler(start, commands=["start"])
+	dp.register_message_handler(echo)
+
+async def process_event(update, dp: Dispatcher):
+
+	Bot.set_current(dp.bot)
+	await dp.process_update(update)
+
+# Selectel serverless entry point
+async def main(**kwargs):
+	bot = Bot(os.environ.get("1592235483:AAGCYWw0cA0CTp6k_J3XYtFPZ4esN8wCTos"))
+	dp = Dispatcher(bot)
+
+	await register_handlers(dp)
+
+	update = types.Update.to_object(kwargs)
+	await process_event(update, dp)
+
+	return 'ok'
